214c03b5efa72e2f0d7f78f8fe818ad7a2d4abfa
Nnrlka <cosspakbrawl@gmail.com>
2026-04-25 13:51:30 +0500

    Добавьте файлы проекта.

---
+++ Views/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace GProject.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
