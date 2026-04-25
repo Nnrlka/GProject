6f3a56a228fabd6ee9f8c9b5786601bf08b01f16
Nnrlka <cosspakbrawl@gmail.com>
2026-04-25 14:53:57 +0500

    Создан класс Transactions.cs

--- Models/Transactions.cs
+++ Models/Transactions.cs
@@ -14,5 +14,6 @@ namespace GProject.Models
         public decimal amount { get;set; }
         public DateTime Date { get;set; }
         public string Category { get;set; } = string.Empty;
+
     }
 }
