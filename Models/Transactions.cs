b27366d8e0a9cd46461b3b3beb03b4f8fb0ab7f6
Nnrlka <cosspakbrawl@gmail.com>
2026-04-25 14:52:36 +0500

    Создал Папку Services а в ней класс DatabaseService.cs Создан класс Transactions.cs заполнен базовыми значениями

---
+++ Models/Transactions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GProject.Models
+{
+    internal class Transactions
+    {
+        public int Id { get;set; }
+        public string Type { get;set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public decimal amount { get;set; }
+        public DateTime Date { get;set; }
+        public string Category { get;set; } = string.Empty;
+    }
+}
---
+++ Services/DatabaseService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using GProject.Models;
+using Npgsql;
+
+
+namespace GProject.Services
+{
+    public class DatabaseService
+    {
+        private readonly string _connectionString;
+
+        public DatabaseService()
+        {
+            _connectionString = "Host=localhost; Port=5432; Database=FinanceDB; Username=postgres; password=postgres";
+        }
+    }
+}
